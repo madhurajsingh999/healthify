b19d4acf078d2712ede55f148efe503ebd664c8d
Anil tripathi <aniltripathi@Anils-MacBook-Pro-2.local>
2024-11-06 06:43:48 +0530

    reward and challenge service module added

---
+++ healthify-reward/src/main/java/com/healthify/HealthifyRewardApplication.java
@@ -0,0 +1,15 @@
+package com.healthify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HealthifyRewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthifyRewardApplication.class, args);
+	}
+
+}
