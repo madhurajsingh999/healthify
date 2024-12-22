27b2da10c2d6b5d90961528fab23a2048514fd6b
Anil tripathi <aniltripathi@Anils-MacBook-Pro-2.local>
2024-12-23 05:04:04 +0530

    updates

---
+++ healthify-gateway/src/main/java/com/healthify/controller/OpenApiController.java
@@ -0,0 +1,18 @@
+package com.healthify.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Controller
+public class OpenApiController {
+    
+    @RequestMapping("/")
+    public String openSwaggerUI() {
+        log.info("DOC Controller");
+        return "redirect:/swagger-ui/index.html";
+    }
+}
